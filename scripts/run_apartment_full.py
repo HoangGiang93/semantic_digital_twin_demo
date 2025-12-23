9737e85f40b8eaac5743411cfcecbd7d1694d8ef
Giang Nguyen <hoanggia@uni-bremen.de>
2025-12-23 01:11:11 +0100

    Update scripts

--- scripts/run_apartment_full.py
+++ scripts/run_apartment_full.py
@@ -5,12 +5,72 @@ import os
 import time
 
 if __name__ == "__main__":
-    scene_path = os.path.join(os.path.dirname(__file__), "..", "assets", "apartment_full.xml")
+    scene_path = os.path.join(
+        os.path.dirname(__file__), "..", "assets", "apartment_full.xml"
+    )
     image_dir = os.path.join(os.path.dirname(__file__), "..", "images")
     world = MJCFParser(scene_path).parse()
     viewer = MultiverseViewer()
-    headless = os.environ.get("CI", "false").lower() == "true" # headless in CI environments
-    multi_sim = MujocoSim(world=world, viewer=viewer, headless=headless, step_size=1E-3, integrator="IMPLICITFAST")
+    viewer.write_objects = {
+        "LeftHand_WristRoot": {
+            "position": [1.5, 2.6, 1],
+            "quaternion": [0, 1, 0, 0],
+        },
+        "LeftHand_ThumbTip": {
+            "position": [1.5785, 2.59142, 1.0835],
+            "quaternion": [-0.653282, 0.653282, 0.270598, 0.270598],
+        },
+        "LeftHand_IndexTip": {
+            "position": [1.665, 2.6, 1.033],
+            "quaternion": [0, 1, 0, 0],
+        },
+        "LeftHand_MiddleTip": {
+            "position": [1.669, 2.6, 1.011],
+            "quaternion": [0, 1, 0, 0],
+        },
+        "LeftHand_RingTip": {
+            "position": [1.665, 2.6, 0.989],
+            "quaternion": [0, 1, 0, 0],
+        },
+        "LeftHand_PinkyTip": {
+            "position": [1.6565, 2.6, 0.967],
+            "quaternion": [0, 1, 0, 0],
+        },
+        "RightHand_WristRoot": {
+            "position": [1.5, 2.4, 1],
+            "quaternion": [0, 0, 0, 1],
+        },
+        "RightHand_ThumbTip": {
+            "position": [1.5785, 2.40858, 1.0835],
+            "quaternion": [0.270598, -0.270598, 0.653282, 0.653282],
+        },
+        "RightHand_IndexTip": {
+            "position": [1.665, 2.4, 1.033],
+            "quaternion": [0, 0, 0, 1],
+        },
+        "RightHand_MiddleTip": {
+            "position": [1.669, 2.4, 1.011],
+            "quaternion": [0, 0, 0, 1],
+        },
+        "RightHand_RingTip": {
+            "position": [1.665, 2.4, 0.989],
+            "quaternion": [0, 0, 0, 1],
+        },
+        "RightHand_PinkyTip": {
+            "position": [1.6565, 2.4, 0.967],
+            "quaternion": [1.0, 0.0, 0.0, 0.0],
+        },
+    }
+    headless = (
+        os.environ.get("CI", "false").lower() == "true"
+    )  # headless in CI environments
+    multi_sim = MujocoSim(
+        world=world,
+        viewer=viewer,
+        headless=headless,
+        step_size=1e-3,
+        integrator="IMPLICITFAST",
+    )
     multi_sim.start_simulation()
 
     print("Wait 1s...")
